a87315486cf4fce19588fa1f6f88f7bea10fd607
Robb Knight <desk@rknight.me>
2026-05-13 10:27:16 +0100

    this is working good

--- _data/mildliners.js
+++ _data/mildliners.js
@@ -1,46 +1,52 @@
 const all = {
-	PINK: { name: 'Pink', color: 'ffb6d9', code: 'WKT7-MP' },
-	ORANGE: { name: 'Orange', color: 'ffdfb5', code: 'WKT7-MOR' },
-	YELLOW: { name: 'Yellow', color: 'f0f4a3', code: 'WKT7-MY' },
-	BLUE: { name: 'Blue', color: '98d5e8', code: 'WKT7-MBL' },
-	BLUE_GREEN: { name: 'Blue Green', color: 'a1d0ca', code: 'WKT7-MBG' },
-	GREEN: { name: 'Green', color: 'b0d492', code: 'WKT7-MG' },
-	DARK_BLUE: { name: 'Dark Blue', color: '8ea7c8', code: 'WKT7-MDB' },
-	GRAY: { name: 'Gray', color: 'bdc7c4', code: 'WKT7-MGR' },
-	VIOLET: { name: 'Violet', color: 'b7abc8', code: 'WKT7-MVI' },
-	RED: { name: 'Red', color: 'dd7e92', code: 'WKT7-MR' },
-	SMOKE_BLUE: { name: 'Smoke Blue', color: 'a9d0ce', code: 'WKT7-MSB' },
-	VERMILLION: { name: 'Vermillion', color: 'ffa488', code: 'WKT7-MVE' },
-	GOLD: { name: 'Gold', color: 'fcd97d', code: 'WKT7-MGO' },
-	MAGENTA: { name: 'Magenta', color: 'e27fd2', code: 'WKT7-MMZ' },
-	BROWN: { name: 'Brown', color: 'ca9e8b', code: 'WKT7-ME' },
-	FUCHSIA: { name: 'Fuchsia', color: 'f3badf', code: 'WKT7-MFU' },
-	MARIGOLD: { name: 'Marigold', color: 'e26650', code: 'WKT7-MMGO' },
-	CITRUS_GREEN: { name: 'Citrus Green', color: 'e3e766', code: 'WKT7-MCG' },
-	SUMMER_GREEN: { name: 'Summer Green', color: '64bda7', code: 'WKT7-MSG' },
-	LAVENDER: { name: 'Lavender', color: '64649e', code: 'WKT7-MLV' },
-	LEMON_YELLOW: { name: 'Lemon Yellow', color: 'fdf284', code: 'WKT7-MLY' },
-	APRICOT: { name: 'Apricot', color: 'f2b071', code: 'WKT7-MAP' },
-	CORAL_PINK: { name: 'Coral Pink', color: 'feb2b8', code: 'WKT7-MCOP' },
-	CYAN: { name: 'Cyan', color: '7bc9d3', code: 'WKT7-MCYA' },
-	DARK_GRAY: { name: 'Dark Gray', color: '83827b', code: 'WKT7-MDGR' },
-	COPPER: { name: 'Copper', color: 'dfa16c', note: '(US only)', code: '' },
-	BEIGE: { name: 'Beige', color: 'd4bd8d', code: 'WKT7-MBE' },
-	CREAM: { name: 'Cream', color: 'f7e4bb', code: 'WKT7-MCR' },
-	COOL_GRAY: { name: 'Cool Gray', color: 'd2d2cd', code: 'WKT7-MCGR' },
-	OLIVE: { name: 'Olive', color: 'd8cf96', code: 'WKT7-MOL' },
-	DUSTY_PINK: { name: 'Dusty Pink', color: 'fcb9c2', note: '(Japan only)', code: 'WKT7-MDP' },
-	HONEY_ORANGE: { name: 'Honey Orange', color: 'f3cca3', code: 'WKT7-MHOR' },
-	SHERBET_YELLOW: { name: 'Sherbet Yellow', color: 'f2ee8e', code: 'WKT7-MSY' },
-	SODA_BLUE: { name: 'Soda Blue', color: 'a1d7e1', code: 'WKT7-MSOB' },
-	LILAC: { name: 'Lilac', color: 'dbc1d3', code: 'WKT7-MLL' },
-	BABY_PINK: { name: 'Baby Pink', color: 'f3c2ca', code: 'WKT7-MBP' },
-	IRIS: { name: 'Iris', color: 'cdc4d6', code: 'WKT7-MIR' },
-	SMOKE_RED: { name: 'Smoke Red', color: 'e5bdc5', code: 'WKT7-MSR' },
-	MIMOSA_YELLOW: { name: 'Mimosa Yellow', color: 'd3ce99', code: 'WKT7-MMY' },
-	MINT: { name: 'Mint', color: 'c5d7d0', code: 'WKT7-MM' },
-	MOSS_GREEN: { name: 'Moss Green', color: '96bab6', code: 'WKT7-MMOG' },
+	PINK: { name: 'Pink', color: 'ffb6d9', code: 'MP' },
+	ORANGE: { name: 'Orange', color: 'ffdfb5', code: 'MOR' },
+	YELLOW: { name: 'Yellow', color: 'f0f4a3', code: 'MY' },
+	BLUE: { name: 'Blue', color: '98d5e8', code: 'MBL' },
+	BLUE_GREEN: { name: 'Blue Green', color: 'a1d0ca', code: 'MBG' },
+	GREEN: { name: 'Green', color: 'b0d492', code: 'MG' },
+	DARK_BLUE: { name: 'Dark Blue', color: '8ea7c8', code: 'MDB' },
+	GRAY: { name: 'Gray', color: 'bdc7c4', code: 'MGR' },
+	VIOLET: { name: 'Violet', color: 'b7abc8', code: 'MVI' },
+	RED: { name: 'Red', color: 'dd7e92', code: 'MR' },
+	SMOKE_BLUE: { name: 'Smoke Blue', color: 'a9d0ce', code: 'MSB' },
+	VERMILLION: { name: 'Vermillion', color: 'ffa488', code: 'MVE' },
+	GOLD: { name: 'Gold', color: 'fcd97d', code: 'MGO' },
+	MAGENTA: { name: 'Magenta', color: 'e27fd2', code: 'MMZ' },
+	BROWN: { name: 'Brown', color: 'ca9e8b', code: 'ME' },
+	FUCHSIA: { name: 'Fuchsia', color: 'f3badf', code: 'MFU' },
+	MARIGOLD: { name: 'Marigold', color: 'e26650', code: 'MMGO' },
+	CITRUS_GREEN: { name: 'Citrus Green', color: 'e3e766', code: 'MCG' },
+	SUMMER_GREEN: { name: 'Summer Green', color: '64bda7', code: 'MSG' },
+	LAVENDER: { name: 'Lavender', color: '64649e', code: 'MLV' },
+	LEMON_YELLOW: { name: 'Lemon Yellow', color: 'fdf284', code: 'MLY' },
+	APRICOT: { name: 'Apricot', color: 'f2b071', code: 'MAP' },
+	CORAL_PINK: { name: 'Coral Pink', color: 'feb2b8', code: 'MCOP' },
+	CYAN: { name: 'Cyan', color: '7bc9d3', code: 'MCYA' },
+	DARK_GRAY: { name: 'Dark Gray', color: '83827b', code: 'MDGR' },
+	COPPER: { name: 'Copper', color: 'dfa16c', code: null },
+	BEIGE: { name: 'Beige', color: 'd4bd8d', code: 'MBE' },
+	CREAM: { name: 'Cream', color: 'f7e4bb', code: 'MCR' },
+	COOL_GRAY: { name: 'Cool Gray', color: 'd2d2cd', code: 'MCGR' },
+	OLIVE: { name: 'Olive', color: 'd8cf96', code: 'MOL' },
+	DUSTY_PINK: { name: 'Dusty Pink', color: 'fcb9c2', code: 'MDP' },
+	HONEY_ORANGE: { name: 'Honey Orange', color: 'f3cca3', code: 'MHOR' },
+	SHERBET_YELLOW: { name: 'Sherbet Yellow', color: 'f2ee8e', code: 'MSY' },
+	SODA_BLUE: { name: 'Soda Blue', color: 'a1d7e1', code: 'MSOB' },
+	LILAC: { name: 'Lilac', color: 'dbc1d3', code: 'MLL' },
+	BABY_PINK: { name: 'Baby Pink', color: 'f3c2ca', code: 'MBP' },
+	IRIS: { name: 'Iris', color: 'cdc4d6', code: 'MIR' },
+	SMOKE_RED: { name: 'Smoke Red', color: 'e5bdc5', code: 'MSR' },
+	MIMOSA_YELLOW: { name: 'Mimosa Yellow', color: 'd3ce99', code: 'MMY' },
+	MINT: { name: 'Mint', color: 'c5d7d0', code: 'MM' },
+	MOSS_GREEN: { name: 'Moss Green', color: '96bab6', code: 'MMOG' },
 }
+
+// codes
+// WKT7 - highlighter
+// WYT9 - fine
+// WKS23 - Q1/2/etc
+// WFT8 - brush
 
 const stepsExample = [
     '10% { color: hsl(360, 65%, 45%); }',
@@ -61,8 +67,178 @@ for (let i = 0; i < Object.keys(all).length; i++) {
     steps.push(`${percentage}% { color: #${color}; }`)
 }
 
+let data = [
+    {
+        name: 'Mildliner',
+        prefix: 'WKT7',
+        sets: {
+            Fluorescent: [
+                all.PINK, all.ORANGE, all.YELLOW, all.BLUE, all.BLUE_GREEN,
+            ],
+            'Cool and Refined': [
+                all.GREEN, all.DARK_BLUE, all.GRAY, all.VIOLET, all.RED,
+            ],
+            Warm: [
+                all.SMOKE_BLUE, all.VERMILLION, all.GOLD, all.MAGENTA, all.BROWN,
+            ],
+            'Refresh Bright': [
+                all.FUCHSIA, all.MARIGOLD, all.CITRUS_GREEN, all.SUMMER_GREEN, all.LAVENDER,
+            ],
+            Friendly: [
+                all.LEMON_YELLOW, all.APRICOT, all.CORAL_PINK, all.CYAN, all.DARK_GRAY,
+            ],
+            'Neutral (US) / Natural (JP)': [
+                all.BEIGE, all.CREAM, all.COOL_GRAY, all.OLIVE, { ...all.DUSTY_PINK, note: 'Japan only' }, { ...all.COPPER, note: 'US only' },
+            ],
+            Gentle: [
+                all.HONEY_ORANGE, all.SHERBET_YELLOW, all.SODA_BLUE, all.LILAC, all.BABY_PINK,
+            ],
+            Calm: [
+                all.IRIS, all.SMOKE_RED, all.MIMOSA_YELLOW, all.MINT, all.MOSS_GREEN,
+            ]
+        }
+    },
+    {
+        name: 'Brush',
+        prefix: 'WFT8',
+        sets: {
+            Fluorescent: [
+                all.PINK, all.ORANGE, all.YELLOW, all.BLUE, all.BLUE_GREEN,
+            ],
+            'Cool and Refined': [
+                all.GREEN, all.DARK_BLUE, all.GRAY, all.VIOLET, all.RED,
+            ],
+            Warm: [
+                all.SMOKE_BLUE, all.VERMILLION, all.GOLD, all.MAGENTA, all.BROWN,
+            ],
+            'Refresh Bright': [
+                all.FUCHSIA, all.MARIGOLD, all.CITRUS_GREEN, all.SUMMER_GREEN, all.LAVENDER,
+            ],
+            Friendly: [
+                all.LEMON_YELLOW, all.APRICOT, all.CORAL_PINK, all.CYAN, all.DARK_GRAY,
+            ],
+        },
+    },
+    {
+        name: 'Fine',
+        prefix: 'WYT9',
+        sets: {
+            A: [
+                all.GRAY, all.BROWN, all.DARK_BLUE, all.RED, all.DARK_GRAY,    
+            ],
+            B: [
+                all.GOLD, all.APRICOT, all.FUCHSIA, all.VIOLET, all.SUMMER_GREEN,
+            ],
+        },
+    },
+    {
+        name: 'Dot',
+        prefix: null,
+        sets: {
+            'Ten Pack': [
+                all.COPPER,
+                all.GRAY,
+                all.LAVENDER,
+                all.CYAN,
+                all.SUMMER_GREEN,
+                all.GREEN,
+                all.GOLD,
+                all.APRICOT,
+                all.RED,
+                all.FUCHSIA,
+            ]
+        }
+    },
+    {
+        name: 'Fragrance',
+        prefix: 'WKT7-FR',
+        sets: {
+            'Six Pack': [
+                { ...all.SODA_BLUE, note: 'Cotton scent' },
+                { ...all.OLIVE, note: 'Green scent' },
+                { ...all.SHERBET_YELLOW, note: 'Citrus scent' },
+                { ...all.CITRUS_GREEN, note: 'White Bloom scent' },
+                { ...all.COOL_GRAY, note: 'Wood scent' },
+                { ...all.BEIGE, note: 'Flower Bouquet scent' },
+            ]
+        }
+    },
+    {
+        name: 'Stamp',
+        prefix: null,
+        sets: {
+            'Five Pack': [
+                { ...all.GOLD, icon: 'stamp-star' },
+                { ...all.APRICOT, icon: 'stamp-arrow' },
+                { ...all.CYAN, icon: 'stamp-star' },
+                { ...all.MAGENTA, icon: 'stamp-heart' },
+                { ...all.MARIGOLD, icon: 'stamp-flower' },
+            ]
+        }
+    },
+    {
+        name: 'Colour',
+        prefix: null,
+        sets: {
+            Reds: [
+               all. RED, all.MARIGOLD, all.SMOKE_RED, all.CORAL_PINK, all.PINK, all.BABY_PINK, all.DUSTY_PINK,
+            ],
+            Oranges: [
+                all.VERMILLION, all.APRICOT, all.ORANGE, all.HONEY_ORANGE,
+            ],
+            Yellows: [
+                all.GOLD, all.LEMON_YELLOW, all.SHERBET_YELLOW, all.MIMOSA_YELLOW, all.YELLOW,
+            ],
+            Greens: [
+                all.CITRUS_GREEN, all.GREEN, all.OLIVE, all.BLUE_GREEN, all.SUMMER_GREEN, all.MOSS_GREEN, all.MINT,
+            ],
+            Blues: [
+                all.CYAN, all.SMOKE_BLUE, all.DARK_BLUE, all.BLUE, all.SODA_BLUE,
+            ],
+            Purples: [
+                all.FUCHSIA, all.LAVENDER, all.VIOLET, all.MAGENTA, all.LILAC, all.IRIS,
+            ],
+            Browns: [
+                all.BEIGE, all.COPPER, all.BROWN, all.CREAM,
+            ],
+            Grays: [
+                all.COOL_GRAY, all.GRAY, all.DARK_GRAY,
+            ],
+        }
+    }
+]
+
+const types = [
+    'Mildliner',
+    'Brush',
+    'Fine',
+    'Dot',
+    'Mix',
+    'Stamp',
+    'Fragrance',
+    'Colour',
+]
+
+let colourData = {}
+Object.values(all).forEach(c => colourData[c.name] = [])
+
+data = data.map(type => {
+    let colours = []
+    Object.values(type.sets).forEach(cl => {
+        cl.forEach(c => {
+            colours.push(c.name)
+            colourData[c.name].push(type.name)
+        })
+    })
+    type.colours = [...new Set(colours)]
+    return type
+})
+
 module.exports = function () {
     return {
+        types,
+        data,
+        colourData,
         cssSteps: steps.join('\n'),
         all: Object.values(all),
         brushSets: [
